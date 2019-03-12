a5d1edd0a25821a6c1ebacbd2161e62b9c713cac
utilisateur <utilisateur@classmob-ftlv09.univ-tln.fr>
2019-03-12 11:37:05 +0100

    pages kevin

---
+++ pages/user_create.php
@@ -0,0 +1,70 @@
+<section class="pt-5 pb-5 bg-dark inner-header">
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 text-center">
+        <div  class="ratio img-responsive img-circle" style="background-image: url(./img/pingouin.jpg);">
+            <h1 class="mt-0 mb-3 text-white">Create User</h1>
+        </div>
+        </div>
+    </div>
+</div>
+</section>
+
+<div class="container">
+<br>  <p class="text-center">More bootstrap 4 components on <a href="http://bootstrap-ecommerce.com/"> Bootstrap-ecommerce.com</a></p>
+<hr>
+
+
+
+
+
+<div class="card bg-light">
+<article class="card-body mx-auto" style="max-width: 400px;">
+	<form>
+	<div class="form-group input-group">
+		<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+		 </div>
+        <input name="" class="form-control" placeholder="Full name" type="text">
+    </div> <!-- form-group// -->
+    
+    <div class="form-group input-group">
+		<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+		 </div>
+        <input name="" class="form-control" placeholder="First name" type="text">
+    </div> <!-- form-group// -->
+    
+    
+    <div class="form-group input-group">
+    	<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-envelope"></i> </span>
+		 </div>
+        <input name="" class="form-control" placeholder="Email address" type="email">
+    </div> <!-- form-group// -->
+    
+    <div class="form-group input-group">
+    	<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-birthday-cake"></i> </span>
+		 </div>
+        <input name="" class="form-control" placeholder="Birthday date" type="birthday">
+    </div> <!-- form-group// -->
+    
+    
+     
+    
+ 
+                                      
+    <div class="form-group">
+        <button type="submit" class="btn btn-primary btn-block"> Create User  </button>
+    </div> <!-- form-group// -->                                                                      
+</form>
+</article>
+</div> <!-- card.// -->
+
+</div> 
+<!--container end.//-->
+
+
+
+</article>
---
+++ pages/user_view.php
@@ -0,0 +1,26 @@
+<div class="container">
+    <div class="row col-md-6 col-md-offset-2 custyle">
+    <table class="table table-striped custab">
+    <thead>
+    <a href="#" class="btn btn-primary btn-xs pull-right"><b>+</b> Add new categories</a>
+        <tr>
+            <th>ID</th>
+            <th>Prenom</th>
+            <th>Nom</th>
+            <th>Email</th>
+            <th>DDN</th>
+            <th class="text-center">Action</th>
+        </tr>
+    </thead>
+            <tr>
+                <td>1</td>
+                <td>Kevin</td>
+                <td>BIGOT</td>
+                <th>bkevin83@hotmail.fr</th>
+                <th>19/01/1997</th>
+                <td class="text-center"><a class='btn btn-info btn-xs' href="#"><span class="glyphicon glyphicon-edit"></span> Edit</a> <a href="#" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-remove"></span> Del</a></td>
+            </tr>
+           
+    </table>
+    </div>
+</div>
